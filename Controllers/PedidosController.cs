000df02cd07b87d603d2cc61b1b5e9e717fba9ea
Oscar <odurancuello@gmail.com>
2021-10-19 23:44:28 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/PedidosController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IDS311_2021_03_Equipo_1.Models;
+
+namespace IDS311_2021_03_Equipo_1.Controllers
+{
+    public class PedidosController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Pedidos
+        public ActionResult Index()
+        {
+            var pedido = db.Pedido.Include(p => p.Producto).Include(p => p.User);
+            return View(pedido.ToList());
+        }
+
+        // GET: Pedidos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedido.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedido);
+        }
+
+        // GET: Pedidos/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProductoId = new SelectList(db.Producto, "Id", "Name");
+            ViewBag.UserId = new SelectList(db.User, "Id", "Username");
+            return View();
+        }
+
+        // POST: Pedidos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ReceiverName,ReceiverLastName,ReceiverAddress,ReceiverPhoneNumber,ProductoId,UserId,TimeCreated,ETA,Status")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pedido.Add(pedido);
+                pedido.TimeCreated = DateTime.Now.ToString();
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProductoId = new SelectList(db.Producto, "Id", "Name", pedido.ProductoId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "Username", pedido.UserId);
+            return View(pedido);
+        }
+
+        // GET: Pedidos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedido.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProductoId = new SelectList(db.Producto, "Id", "Name", pedido.ProductoId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "Username", pedido.UserId);
+            return View(pedido);
+        }
+
+        // POST: Pedidos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ReceiverName,ReceiverLastName,ReceiverAddress,ReceiverPhoneNumber,ProductoId,UserId,TimeCreated,ETA,Status")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pedido).State = EntityState.Modified;
+                pedido.TimeCreated.ToString();
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProductoId = new SelectList(db.Producto, "Id", "Name", pedido.ProductoId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "Username", pedido.UserId);
+            return View(pedido);
+        }
+
+        // GET: Pedidos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedido.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedido);
+        }
+
+        // POST: Pedidos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pedido pedido = db.Pedido.Find(id);
+            db.Pedido.Remove(pedido);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IDS311_2021_03_Equipo_1.MvcApplication" Language="C#" %>
